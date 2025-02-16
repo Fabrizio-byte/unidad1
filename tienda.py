fc6e3eacec2a101039bc361a18bc3898a7da7ca5
Fabrizio-byte <cf.albant@uea.edu.ec>
2025-02-16 12:53:25 -0500

    Add files via upload

---
+++ tienda.py
@@ -0,0 +1,40 @@
+#inventario/
+#├── producto.py
+#├── inventario.py
+#└── main.py
+# producto.py
+class Producto:
+    def __init__(self, id_producto, nombre, cantidad, precio):
+        self.id_producto = id_producto
+        self.nombre = nombre
+        self.cantidad = cantidad
+        self.precio = precio
+
+    # Getters y setters para cada atributo
+    def get_id(self):
+        return self.id_producto
+
+    def set_id(self, id_producto):
+        self.id_producto = id_producto
+
+    def get_nombre(self):
+        return self.nombre
+
+    def set_nombre(self, nombre):
+        self.nombre = nombre
+
+    def get_cantidad(self):
+        return self.cantidad
+
+    def set_cantidad(self, cantidad):
+        self.cantidad = cantidad
+
+    def get_precio(self):
+        return self.precio
+
+    def set_precio(self, precio):
+        self.precio = precio
+
+    def __str__(self):  # Para mostrar información del producto de forma legible
+        return f"ID: {self.id_producto}, Nombre: {self.nombre}, Cantidad: {self.cantidad}, Precio: {self.precio}"
+
